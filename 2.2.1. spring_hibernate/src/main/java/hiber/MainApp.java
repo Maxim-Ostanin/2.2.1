2d8b234b3947a307c006f13c43bc5a7b1e899b2b
Maxim Ostanin <ostanin.maks@gmail.com>
2023-07-05 16:50:50 +0300

    second commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -21,10 +21,10 @@ public class MainApp {
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"), new Car("Model1",1));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"), new Car("Model2",2));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"), new Car("Model3",3));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"), new Car("Model4",4));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Model1",1)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Model2",2)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("Model3",3)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("Model4",4)));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -36,8 +36,8 @@ public class MainApp {
          System.out.println();
       }
 
-      List<User> usersCarsOwners = userService.listUsersWithCar("Model1", 1);
-      for (User user : usersCarsOwners) {
+      List<User> usersWithCar = userService.listUsersWithCar("Model1", 1);
+      for (User user : usersWithCar) {
          System.out.println("Id = "+user.getId());
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
